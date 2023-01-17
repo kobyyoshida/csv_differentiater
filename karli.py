8161e65c9a2ca297674d0a299dd24fb41b63cd7e
kobyyoshida <koyoshida@chapman.edu>
2023-01-17 14:54:40 -0800

    added line counter

--- karli.py
+++ karli.py
@@ -6,9 +6,15 @@ with open('example.csv', 'r') as t1, open('example2.csv', 'r') as t2:
     fileone = t1.readlines()
     filetwo = t2.readlines()
 
-with open('update.csv', 'w') as outFile:
-    for line in filetwo:
-        for letter in line:
-            print(letter)
-        #if line not in fileone:
-        #    outFile.write(line)
+    for count,line in enumerate(filetwo):
+        if line not in fileone:
+           print("Line: ", count+1)
+           print(line)
+
+# with open('update.csv', 'w') as outFile:
+
+#     for line in filetwo:
+#         #for letter in line:
+#         if line not in fileone:
+#            print(line)
+#            outFile.write(line)
